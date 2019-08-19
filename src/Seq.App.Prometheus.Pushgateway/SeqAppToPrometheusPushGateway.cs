0434860559c6c56915ba573aedf400842d24c4ce
DU00556591 <DU00556591@INBASDLP06496>
2019-08-19 10:09:06 +0530

    changes done

--- src/Seq.App.Prometheus.Pushgateway/SeqAppToPrometheusPushGateway.cs
+++ src/Seq.App.Prometheus.Pushgateway/SeqAppToPrometheusPushGateway.cs
@@ -52,9 +52,9 @@ namespace Seq.App.Prometheus.Pushgateway
             var pushgatewayCounterData = FormatTemplate(evt, additionalPropertiesList);
 
 
-            var customGauge = Metrics.WithCustomRegistry(registry).CreateGauge(GaugeName, "To track the seq errors", new[] { "ApplicationName" });
+            var customGauge = Metrics.WithCustomRegistry(registry).CreateGauge(GaugeName, "To track the seq errors", true, new[] { "ApplicationName" });
             var gaugeValue = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-            customGauge.Labels(pushgatewayCounterData.ResourceName).Set(gaugeValue);
+            customGauge.Labels(pushgatewayCounterData.ResourceName).Set(gaugeValue, DateTime.UtcNow);
         }
 
         public static PushgatewayCounterData FormatTemplate(Event<LogEventData> evt, List<string> applicationNameKeyList)
