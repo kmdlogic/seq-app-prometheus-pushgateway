12165fe75117ca5b6b6669ff02567970113e9b2f
DU00556591 <DU00556591@INBASDLP06496>
2019-09-05 17:27:43 +0530

    added code for reset

--- src/Seq.App.Prometheus.Pushgateway/SeqAppToPrometheusPushGateway.cs
+++ src/Seq.App.Prometheus.Pushgateway/SeqAppToPrometheusPushGateway.cs
@@ -40,8 +40,8 @@ namespace Seq.App.Prometheus.Pushgateway
         public void On(Event<LogEventData> evt)
         {
             var counter = Metrics.CreateCounter(CounterName, "To keep the count of no of times a particular error coming in a module.");
-            counter.Inc();
             counter.Reset();
+            counter.Inc();            
         }
     }
 }
